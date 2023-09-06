a86d911e986b88d1953d6a90dee8c4513f5c461e
Prabuddhika Mayurapaksha <prabuddhika1996@gmail.com>
2023-09-06 15:56:53 +0530

    add repo to user impl

--- src/main/java/com/example/consultantapp/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/example/consultantapp/ServiceImpl/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.consultantapp.ServiceImpl;
 
+import com.example.consultantapp.Repository.UserRepository;
 import com.example.consultantapp.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
 }
